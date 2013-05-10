3be46accead9bd09412618f3f5188c14a7d4e3f0
Tom McFarlin <tom@tommcfarlin.com>
2013-05-10 07:33:53 -0400

    adding braces around the conditional in `uninstall.php`

--- plugin-boilerplate/uninstall.php
+++ plugin-boilerplate/uninstall.php
@@ -7,7 +7,8 @@
  */
 
 // If uninstall, not called from WordPress exit
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
+}
 
 // TODO:	Define uninstall functionality here
