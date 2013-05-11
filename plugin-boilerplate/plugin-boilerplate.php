a2987b80ca24fda84c8747876e172c7b82145d7e
Tom McFarlin <tom@tommcfarlin.com>
2013-05-11 17:03:07 -0400

    adding license.txt and removing it from the header.
    fixes #42

--- plugin-boilerplate/plugin-boilerplate.php
+++ plugin-boilerplate/plugin-boilerplate.php
@@ -7,23 +7,10 @@ Version: 1.0.0
 Author: TODO
 Author URI: TODO
 Author Email: TODO
-License:
+License: GNU General Public License Unversioned
+License URI: http://www.gnu.org/licenses/gpl.txt
 
 Copyright 2013 TODO (email@domain.com)
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License, version 2, as
-published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
 */
 
 /*
