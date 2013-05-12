49abd665efe114f2723020786ff0e1ee36c2ce8c
Tom McFarlin <tom@tommcfarlin.com>
2013-05-12 08:09:17 -0400

    adding the domain path tag to the header of the plugin file
    Fixes #40

--- plugin-boilerplate/plugin-boilerplate.php
+++ plugin-boilerplate/plugin-boilerplate.php
@@ -10,7 +10,7 @@ Author Email: TODO
 Text Domain: plugin-name-locale
 License: GPL-2.0+
 License URI: http://www.gnu.org/licenses/gpl-2.0.txt
-
+Domain Path: /lang/
 Copyright 2013 TODO (email@domain.com)
 */
 
