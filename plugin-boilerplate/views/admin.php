45f6b3e6919605007f2a3f791b0c653bb806af2a
Tom McFarlin <tom@tommcfarlin.com>
2013-05-12 08:56:48 -0400

    adding docblocks to the views

--- plugin-boilerplate/views/admin.php
+++ plugin-boilerplate/views/admin.php
@@ -1,8 +1,24 @@
+<?php
+/**
+ * Represents the view for the administration dashboard.
+ *
+ * This includes the header, options, and other information that should provide
+ * The User Interface to the end user.
+ *
+ * @package    PluginName
+ * @subpackage views
+ * @author     TODO
+ * @license    GPL-2.0+
+ * @link       TODO
+ * @version    1.0.0
+ * @since      1.0.0
+ */
+?>
 <div class="wrap">
 
 	<?php screen_icon(); ?>
 	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
-	
+
 	<!-- TODO: Provide markup for your options page here. -->
-	
+
 </div>
--- plugin-boilerplate/views/display.php
+++ plugin-boilerplate/views/display.php
@@ -1,1 +1,17 @@
+<?php
+/**
+ * Represents the view for the public-facing component of the plugin.
+ *
+ * This typically includes any information, if any, that is rendered to the
+ * frontend of the theme when the plugin is activated.
+ *
+ * @package    PluginName
+ * @subpackage views
+ * @author     TODO
+ * @license    GPL-2.0+
+ * @link       TODO
+ * @version    1.0.0
+ * @since      1.0.0
+ */
+?>
 <!-- This file is used to markup the public facing aspect of the plugin. -->
