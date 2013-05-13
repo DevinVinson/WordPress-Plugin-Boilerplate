fca27a1de2156b3b3d3ad7026bbfe6dd47a943a1
Tom McFarlin <tom@tommcfarlin.com>
2013-05-13 13:50:23 -0400

    removing the plugin version constant (since its a property in the class)
    fixes #49

--- plugin-boilerplate/plugin-boilerplate.php
+++ plugin-boilerplate/plugin-boilerplate.php
@@ -29,22 +29,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/*
- * The following constant is used to define a constant for this plugin to make it
- * easier to provide cache-busting functionality on loading stylesheets
- * and JavaScript.
- *
- * After you've defined these constants, do a find/replace on the constants
- * used throughout the rest of this file.
- */
-// TODO: Replace 'PLUGIN_NAME' wih the name of your class
-if ( ! defined( 'PLUGIN_NAME_VERSION' ) ) {
-
-	// TODO: Make sure that this version correspondings to the value in the 'Version' in the header
-	define( 'PLUGIN_NAME_VERSION', '1.0.0' );
-
-}
-
 // TODO: replace `class-plugin-boilerplate.php` with the name of the actual plugin's class file
 require_once( plugin_dir_path( __FILE__ ) . 'class-plugin-boilerplate.php' );
 
