d5fe812b83ea24fdeca4ac123d83040710043b8d
Tom McFarlin <tom@tommcfarlin.com>
2013-05-15 10:16:38 -0400

    removing author email tag and lining up terms
    related #48

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -12,14 +12,13 @@
  *
  * @wordpress-plugin
  * Plugin Name: TODO
- * Plugin URI: TODO
+ * Plugin URI:  TODO
  * Description: TODO
- * Version: 1.0.0
- * Author: TODO
- * Author URI: TODO
- * Author Email: TODO
+ * Version:     1.0.0
+ * Author:      TODO
+ * Author URI:  TODO
  * Text Domain: plugin-name-locale
- * License: GPL-2.0+
+ * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path: /lang/
  */
