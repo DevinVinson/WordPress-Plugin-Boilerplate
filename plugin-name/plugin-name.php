0d968d2c20af65ca9b1005aa036d7de8aaccbb74
Tom McFarlin <tom@tommcfarlin.com>
2013-05-15 10:18:02 -0400

    removing a trailing /
    related #48

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -20,7 +20,7 @@
  * Text Domain: plugin-name-locale
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * Domain Path: /lang/
+ * Domain Path: /lang
  */
 
 // If this file is called directly, abort.
