001cb178aeaa94cf0892bafac31b6ef853cd64b0
Thijs de Zoete <thijsdezoete@gmail.com>
2013-08-20 16:57:05 +0200

    Update old documentation

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -11,17 +11,18 @@
  * @link      http://example.com
  * @copyright 2013 Your Name or Company Name
  *
- * @wordpress-plugin
- * Plugin Name: TODO
- * Plugin URI:  TODO
- * Description: TODO
- * Version:     1.0.0
- * Author:      TODO
- * Author URI:  TODO
- * Text Domain: plugin-name-locale
- * License:     GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * Domain Path: /lang
+ * @wordpress-plugin */
+/*
+Plugin Name: TODO
+Plugin URI:  TODO
+Description: TODO
+Version:     1.0.0
+Author:      TODO
+Author URI:  TODO
+Text Domain: plugin-name-locale
+License:     GPL-2.0+
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+Domain Path: /lang
  */
 
 // If this file is called directly, abort.
