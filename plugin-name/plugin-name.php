b1abaee963fa539aa509de7502aec76abf580634
Tom McFarlin <tom@tommcfarlin.com>
2013-10-16 16:38:25 -0400

    adding a 'GitHub Plugin URI' to the wordpress-plugin header
    Fixes #75

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -22,6 +22,7 @@
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path: /languages
+ * GitHub Plugin URI: https://github.com/<owner>/<repo>
  */
 
 // If this file is called directly, abort.
