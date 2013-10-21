25c4fdcf7008c8b02ca9fb7dc276fa4830409787
Tom McFarlin <tom@tommcfarlin.com>
2013-10-21 07:30:40 -0400

    updating the references to the languages directory as per @grappler's original implementation

--- plugin-name/class-plugin-name.php
+++ plugin-name/class-plugin-name.php
@@ -254,7 +254,7 @@ class Plugin_Name {
 		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
 
 		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
-		load_plugin_textdomain( $domain, FALSE, basename( dirname( __FILE__ ) ) . '/languages' );
+		load_plugin_textdomain( $domain, FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
 
 	}
 
