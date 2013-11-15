0768adb25e5709aaa10ea6905d7e3328f2c1f4c5
James W. Lane III <james.w.lane@fueledbydreams.com>
2013-11-15 05:40:35 -0600

    Removed the trailing slash
    According to http://codex.wordpress.org/Function_Reference/plugin_dir_path, plugin_dir_path gets the filesystem directory path with trailing slash. Even tho two slashes will not cause an error, the extra slash can be removed.

--- plugin-name/public/class-plugin-name.php
+++ plugin-name/public/class-plugin-name.php
@@ -257,7 +257,7 @@ class Plugin_Name {
 		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
 
 		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
-		load_plugin_textdomain( $domain, FALSE, basename( plugin_dir_path( dirname( __FILE__ ) ) ) . '/languages/' );
+		load_plugin_textdomain( $domain, FALSE, basename( plugin_dir_path( dirname( __FILE__ ) ) ) . 'languages/' );
 
 	}
 
