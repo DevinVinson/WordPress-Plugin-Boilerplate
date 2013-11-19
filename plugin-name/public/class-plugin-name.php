ce5d816a4d737cd2a5aa302c904791ef68914f77
Tom McFarlin <tom@tommcfarlin.com>
2013-11-19 17:20:26 -0500

    updating the localization functions to that `load_textdomain` is called.
    Removing `load_plugin_textdomain` and leaving only `load_textdomain`.
    
    This is standard as per new WordPress practices[0].
    
    Ultimately, it makes language files more portable as they are still accessible via WordPress even if the plugin developer did not include them with the plugin itself.
    
    Related #120
    
    [0] https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/commit/d2eb67079b3dd8db76185737a72b5d2efda542ae#commitcomment-4644357
    
    Signed-off-by: Tom McFarlin <tom@tommcfarlin.com>

--- plugin-name/public/class-plugin-name.php
+++ plugin-name/public/class-plugin-name.php
@@ -134,7 +134,6 @@ class Plugin_Name {
 
 					switch_to_blog( $blog_id );
 					self::single_activate();
-
 				}
 
 				restore_current_blog();
@@ -257,8 +256,8 @@ class Plugin_Name {
 		$domain = $this->plugin_slug;
 		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
 
-		load_textdomain( $domain, $domain . '/languages/' . $domain . '-' . $locale . '.mo' );
-		load_plugin_textdomain( $domain, FALSE, basename( plugin_dir_path( dirname( __FILE__ ) ) ) . '/languages/' );
+		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
+		load_plugin_textdomain( $domain, FALSE, basename( plugin_dir_path( dirname( __FILE__ ) ) ) . 'languages/' );
 
 	}
 
