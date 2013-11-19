01319c57e34da3b8d50c4f4585243379414b15e7
Tom McFarlin <tom@tommcfarlin.com>
2013-11-19 17:24:35 -0500

    removing plugin textdomain
    related #120
    
    Signed-off-by: Tom McFarlin <tom@tommcfarlin.com>

--- plugin-name/public/class-plugin-name.php
+++ plugin-name/public/class-plugin-name.php
@@ -257,7 +257,6 @@ class Plugin_Name {
 		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
 
 		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
-		load_plugin_textdomain( $domain, FALSE, basename( plugin_dir_path( dirname( __FILE__ ) ) ) . 'languages/' );
 
 	}
 
