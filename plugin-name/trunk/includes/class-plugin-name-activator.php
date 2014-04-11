b426fbe514df763446a5c1d74aa5546077009153
Tom McFarlin <tom@tommcfarlin.com>
2014-04-11 10:36:13 -0400

    renaming the plugin activation and deactivation classes
    Signed-off-by: Tom McFarlin <tom@tommcfarlin.com>

---
+++ plugin-name/trunk/includes/class-plugin-name-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @TODO: Short description (no period for file headers)
+ *
+ * @TODO: Long description.
+ *
+ * @package   Plugin_Name
+ * @author    Your Name <email@example.com>
+ * @license   GPL-2.0+
+ * @link      http://example.com
+ * @copyright 2014 Your Name or Company Name
+ */
+class Plugin_Name_Activator {
+
+	/**
+	 * @TODO: Short description. (use period)
+	 *
+	 * @TODO: Long description.
+	 *
+	 * @since x.x.x
+	 *
+	 * @param type $var Description.
+	 * @param array $args {
+	 *     Short description about this hash.
+	 *
+	 *     @type type $var Description.
+	 *     @type type $var Description.
+	 * }
+	 * @param type $var Description.
+	 */
+	public static function activate() {
+
+	}
+
+}
