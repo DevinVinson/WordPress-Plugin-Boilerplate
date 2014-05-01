a4fc32fa594b8db588632c1f91ba11fb284ff32d
Tom McFarlin <tom@tommcfarlin.com>
2014-04-30 22:34:19 -0400

    providing a first pass at the docblocks for the admin loader
    eventually, this class will be responsible for defining all of the hooks for the admin-facing side of the plugin

---
+++ plugin-name/trunk/admin/class-plugin-name-admin-loader.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @TODO
+ * Define a short description for what this class does.
+ *
+ * @since   x.x.x
+ * @package TODO
+ */
+
+
+/**
+ * @TODO:
+ * Define a short description for what this class does.
+ *
+ * Define a longer description for the purpose of this class.
+ *
+ * @package   TODO
+ * @author    Your Name <your@email.com>
+ * @license   GPL-2.0+
+ * @link      URL
+ * @copyright 2014 Your Name of Your Company Name
+ */
+class Plugin_Name_Admin_Loader {
+
+	public function run() {
+
+	}
+
+}
