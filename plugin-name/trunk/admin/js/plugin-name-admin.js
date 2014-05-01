deadcc668bc3200b467b8be66d44fb644bb0aab2
Tom McFarlin <tom@tommcfarlin.com>
2014-04-30 22:33:06 -0400

    adding basic documentation for the admin javascript file
    this may eventually be removed and placed into the boilerplate's documentation on the official website, but until that exists, it will remain here.

--- plugin-name/trunk/admin/js/plugin-name-admin.js
+++ plugin-name/trunk/admin/js/plugin-name-admin.js
@@ -1,4 +1,28 @@
 (function( $ ) {
 	'use strict';
 
+	/**
+	 * @TODO
+	 *
+	 * All of the code for your Dashboard-specific JavaScript source
+	 * should reside in this file.
+	 *
+	 * Note that this assume you're going to user jQuery, so it prepares
+	 * the $ function reference to be used within the scope of this
+	 * function.
+	 *
+	 * From here, you're able to define handlers for when the DOM is
+	 * ready:
+	 *
+	 * $(function() {
+	 * });
+	 *
+	 * Or when the window is completely ready:
+	 *
+	 * $( window ).load(function() {
+	 * });
+	 *
+	 * ...and so on.
+	 */
+
 })( jQuery );
