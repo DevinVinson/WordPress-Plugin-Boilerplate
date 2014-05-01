fc2b3109dd63156c321c5abab780bc56c2082ec4
Tom McFarlin <tom@tommcfarlin.com>
2014-04-30 22:56:05 -0400

    defining the loader functionality to be used via dependency injection

---
+++ plugin-name/trunk/public/class-plugin-name-public-loader.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @TODO
+ * Define a short description for what this class does.
+ *
+ * @since   x.x.x
+ * @package TODO
+ */
+
+/**
+ * @TODO
+ * Define a short description for what this class does.
+ *
+ * Define a longer description for the purpose of this class.
+ *
+ * @package   TODO
+ * @author    Your Name <your@email.com>
+ * @license   GPL-2.0+
+ * @link      URL
+ * @copyright 2014 Your Name of Your Company Name
+ */
+class Plugin_Name_Public_Loader {
+
+	/**
+	 * @TODO
+	 * Short description. (use period)
+	 *
+	 * Long description.
+	 *
+	 * @since    1.0.0
+	 * @link     URL
+	 * @param    type    $plugin_name_public    TODO
+	 */
+	public function run( $plugin_name_public ) {
+
+		/**
+		 * @TODO
+		 *
+		 * This function is used to define the various hooks that are used in the
+		 * public-facing side of the plugin. This is achieved via dependency injection
+		 * by passing an instance of Plugin_Name_Public into this class.
+		 *
+		 * Each hook then corresponds to a public function defined within the Plugin_Name_Public
+		 * class.
+		 *
+		 * For example:
+		 *
+		 * add_filter( 'the_content', array( $plugin_name_public, 'display_the_content' ) );
+		 */
+
+	}
+
+}
