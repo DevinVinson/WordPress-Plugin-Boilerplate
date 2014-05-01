71ff4b6ec7515ded65453897df0b216320f1f9a5
Tom McFarlin <tom@tommcfarlin.com>
2014-04-30 22:54:00 -0400

    updating the documentation
    updating the documentation for the core admin plugin file with docblocks and example functions for how they should be used and their relationship to the admin loader

--- plugin-name/trunk/admin/class-plugin-name-admin.php
+++ plugin-name/trunk/admin/class-plugin-name-admin.php
@@ -1,14 +1,80 @@
 <?php
+
 /**
- * @TEAM: http://make.wordpress.org/core/handbook/inline-documentation-standards/php-documentation-standards/#1-functions-and-class-methods
+ * @TODO
+ * Define a short description for what this class does.
  *
- * @package   Plugin_Name
- * @author    Your Name <email@example.com>
- * @license   GPL-2.0+
- * @link      http://example.com
- * @copyright 2014 Your Name or Company Name
+ * @since   x.x.x
+ * @package TODO
  */
 
+/**
+ * @TODO
+ * Define a short description for what this class does.
+ *
+ * Define a longer description for the purpose of this class.
+ *
+ * @package   TODO
+ * @author    Your Name <your@email.com>
+ * @license   GPL-2.0+
+ * @link      URL
+ * @copyright 2014 Your Name of Your Company Name
+ */
 class Plugin_Name_Admin {
+
+	/**
+	 * @TODO
+	 * Short description. (use period)
+	 *
+	 * Long description.
+	 *
+	 * @since    1.0.0
+	 * @link     URL
+	 * @param    integer    $post_id    The ID of the post that will be saved.
+	 */
+	public function save_post( $post_id ) {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Plugin_Name_Admin_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Plugin_Name_Admin_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+	}
+
+	/**
+	 * @TODO
+	 * Short description. (use period)
+	 *
+	 * Long description.
+	 *
+	 * @since    1.0.0
+	 * @link     URL
+	 * @param    string    $content    The content for the post type that's being filtered.
+	 * @return   string                The modified version of the post content.
+	 */
+	public function update_the_content( $content ) {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Plugin_Name_Admin_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Plugin_Name_Admin_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		return $content;
+
+	}
 
 }
