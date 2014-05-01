4b7dde55538143793ee44c74b354069763c5facc
Tom McFarlin <tom@tommcfarlin.com>
2014-04-30 22:54:48 -0400

    defining the basic code comments and the public facing partial

---
+++ plugin-name/trunk/public/partials/plugin-name-public-display.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @TODO
+ * Define a short description for what this file does.
+ *
+ * @since   x.x.x
+ * @package TODO
+ */
+
+/**
+ * @TODO
+ * Define a short description for what this file does.
+ *
+ * Define a longer description for the purpose of this file.
+ *
+ * @package   TODO
+ * @author    Your Name <your@email.com>
+ * @license   GPL-2.0+
+ * @link      URL
+ * @copyright 2014 Your Name of Your Company Name
+ */
+?>
+
+<!-- This file is used to markup the public-facing aspects of the plugin. -->
