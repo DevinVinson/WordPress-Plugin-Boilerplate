5265d1f0aed47103ef89f4b8e9ebb6e39fd4c6b0
Tom McFarlin <tom@tommcfarlin.com>
2014-05-05 00:18:13 -0400

    introducing the single, shared loader class

---
+++ plugin-name/trunk/includes/class-plugin-name-loader.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Define a short description for what this class does (no period)
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ * @author     Your Name <email@example.com>
+ * @license    GPL-2.0+
+ * @link       http://example.com
+ * @copyright  2014 Your Name or Company Name
+ * @since      1.0.0
+ */
+
+/**
+ * Define a short description for what this class does.
+ *
+ * Define a longer description for the purpose of this class.
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ * @author     Your Name <email@example.com>
+ */
+class Plugin_Name_Loader {
+
+	protected $hooks;
+
+	public function __construct() {
+		$this->hooks = array();
+	}
+
+	public function add( $hook, $component, $callback ) {
+
+		$this->hooks[] = array(
+			'hook'      => $hook,
+			'component' => $component,
+			'callback'  => $callback
+		);
+
+	}
+
+	/**
+	 * Short description. (use period)
+	 *
+	 * Long description.
+	 *
+	 * @since    1.0.0
+	 */
+	public function run() {
+
+		/**
+		 * This function is used to define the various hooks that are shared in the
+		 * both the dashboard and the public-facing areas of the plugin. This is
+		 * achieved via dependency injection by passing an instance of Plugin_Name
+		 * into this class.
+		 *
+		 * Each hook then corresponds to a public function defined within the Plugin_Name_Admin
+		 * class.
+		 */
+		 foreach ( $this->hooks as $plugin_name => $hook ) {
+			 add_action( $hook['hook'], array( $hook['component'], $hook['callback'] ) );
+		 }
+
+	}
+
+}
