0fdaeaa67cdb4cc3f37ae28b320c37706ccbc247
Tom McFarlin <tom@tommcfarlin.com>
2014-05-08 18:09:50 -0400

    fixing a small type, documentation the imports, changing the way in which the plugin is instantiated

--- plugin-name/trunk/plugin-name.php
+++ plugin-name/trunk/plugin-name.php
@@ -8,7 +8,7 @@
  * @package   Plugin_Name
  * @author    Your Name or Company Name <email@domain.com>
  * @license   GPL-2.0+
- * @link      http://exmaple.com/plugin-name
+ * @link      http://example.com/plugin-name
  * @copyright 2014 Your Name or Company Name
  *
  * @wordpress-plugin
@@ -30,20 +30,14 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 /**
- * Includes the plugin activation class that runs during plugin activation.
+ * The plugin activation class that runs during plugin activation.
  */
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
 
 /**
- * Includes the plugin deactivation class that runs during plugin deactivation.
+ * The plugin deactivation class that runs during plugin deactivation.
  */
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
-
-/**
- * The base class used to define certain functionality and attributes shared among
- * all shared subclasses of the core plugin file.
- */
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
 
 /** This action is documented in includes/class-plugin-name-activator.php */
 register_activation_hook( __FILE__, array( 'Plugin_Name_Activator', 'activate' ) );
@@ -52,40 +46,47 @@ register_activation_hook( __FILE__, array( 'Plugin_Name_Activator', 'activate' )
 register_activation_hook( __FILE__, array( 'Plugin_Name_Deactivator', 'deactivate' ) );
 
 /**
- * Includes the class responsible for defining the core functionality of the
- * shared components of the plugin.
+ * The class responsible for orchestrating the actions and filters of the
+ * core plugin.
  */
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-loader.php';
-$loader = new Plugin_Name_Loader();
 
 /**
- * Includes the class responsible for defining internationalization functionality
+ * The class responsible for defining internationalization functionality
  * of the plugin.
  */
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-i18n.php';
-$plugin_i18n = new Plugin_Name_i18n();
-$loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
 
 /**
- * TODO
- */
-require_once plugin_dir_path( __FILE__ ) . 'admin/class-plugin-name-admin.php';
-$plugin_name_admin = new Plugin_Name_Admin();
-$loader->add_action( 'admin_enqueue_scripts', $plugin_name_admin, 'enqueue_styles' );
-$loader->add_action( 'admin_enqueue_scripts', $plugin_name_admin, 'enqueue_scripts' );
-
-/**
- * TODO
- */
-require_once plugin_dir_path( __FILE__ ) . 'public/class-plugin-name-public.php';
-$plugin_name_public = new Plugin_Name_Public();
-$loader->add_action( 'wp_enqueue_scripts', $plugin_name_public, 'enqueue_styles' );
-$loader->add_action( 'wp_enqueue_scripts', $plugin_name_public, 'enqueue_scripts' );
-
-/**
- * TODO
+ * The base class used to define certain functionality and attributes used among
+ * the dashboard-specific and public-facing functionality.
  */
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
+
+/**
+ * The class responsible for defining all actions that occur in the Dashboard.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'admin/class-plugin-name-admin.php';
+
+/**
+ * The class responsible for defining all actions that occur in the public-facing
+ * side of the site.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'public/class-plugin-name-public.php';
+
+$loader = new Plugin_Name_Loader();
 $plugin = new Plugin_Name( $loader );
+
+$plugin_i18n = new Plugin_Name_i18n();
+$plugin_i18n->set_domain( $plugin->get_slug() );
+$loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
+
+$plugin_admin = new Plugin_Name_Admin( $plugin->get_version() );
+$loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
+$loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+
+$plugin_public = new Plugin_Name_Public( $plugin->get_version() );
+$loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+$loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 $plugin->run();
