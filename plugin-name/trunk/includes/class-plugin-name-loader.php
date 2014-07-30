48e0a373dd3c5d120e936cf2a4387c2492ca769e
Ulrich Pogson <grapplerulrich@gmail.com>
2014-07-30 16:04:18 +0200

    Remove spaces before code

--- plugin-name/trunk/includes/class-plugin-name-loader.php
+++ plugin-name/trunk/includes/class-plugin-name-loader.php
@@ -121,13 +121,13 @@ class Plugin_Name_Loader {
 	 */
 	public function run() {
 
-		 foreach ( $this->filters as $hook ) {
-			 add_filter( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
-		 }
+		foreach ( $this->filters as $hook ) {
+			add_filter( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
+		}
 
-		 foreach ( $this->actions as $hook ) {
-			 add_action( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
-		 }
+		foreach ( $this->actions as $hook ) {
+			add_action( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
+		}
 
 	}
 
