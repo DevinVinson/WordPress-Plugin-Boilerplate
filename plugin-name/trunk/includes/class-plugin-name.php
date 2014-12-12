f4708bda94bdacd0d8d25014d166f201d793d566
Adrian <softmixt@gmail.com>
2014-12-12 15:30:08 +0100

    Integrate models class
    Use same plugin structure to integrate the new database class ...

--- plugin-name/trunk/includes/class-plugin-name.php
+++ plugin-name/trunk/includes/class-plugin-name.php
@@ -101,6 +101,12 @@ class Plugin_Name {
 		 * core plugin.
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-plugin-name-loader.php';
+		
+		/**
+		 * The class responsible for database interactions
+		 * core plugin.
+		 */
+		require_once plugin_dir_path ( dirname ( __FILE__ ) ) . 'includes/class-plugin-name-models.php';
 
 		/**
 		 * The class responsible for defining internationalization functionality
@@ -140,6 +146,19 @@ class Plugin_Name {
 		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
 
 	}
+	
+	/**
+	 * Fet the class that interact with teh database.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function load_models ()
+	{
+
+		return new Plugin_Name_Models( $this->get_plugin_name () , $this->get_version () );
+
+	}
 
 	/**
 	 * Register all of the hooks related to the dashboard functionality
@@ -150,7 +169,7 @@ class Plugin_Name {
 	 */
 	private function define_admin_hooks() {
 
-		$plugin_admin = new Plugin_Name_Admin( $this->get_plugin_name(), $this->get_version() );
+		$plugin_admin = new Plugin_Name_Admin( $this->get_plugin_name(), $this->get_version(),  , $this->load_models ()  );
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
@@ -166,7 +185,7 @@ class Plugin_Name {
 	 */
 	private function define_public_hooks() {
 
-		$plugin_public = new Plugin_Name_Public( $this->get_plugin_name(), $this->get_version() );
+		$plugin_public = new Plugin_Name_Public( $this->get_plugin_name(), $this->get_version(),  , $this->load_models ()  );
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
