33e1d4cefb7ee9cbabde0019b534b77f38374d31
Robert Harm <robert@harm.co.at>
2015-07-25 17:58:17 +0200

    add enhanced uninstall template

--- plugin-name/uninstall.php
+++ plugin-name/uninstall.php
@@ -6,12 +6,68 @@
  * @author    Your Name <email@example.com>
  * @license   GPL-2.0+
  * @link      http://example.com
- * @copyright 2014 Your Name or Company Name
+ * @copyright 2015 Your Name or Company Name
  */
 
 // If uninstall not called from WordPress, then exit
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
-
-// @TODO: Define uninstall functionality here
+if (is_multisite()) {
+	global $wpdb;
+	$blogs = $wpdb->get_results("SELECT blog_id FROM {$wpdb->blogs}", ARRAY_A);
+		/* @TODO: delete all transient, options and files you may have added 
+		delete_transient( 'TRANSIENT_NAME' );
+		delete_option('OPTION_NAME');
+		//info: remove custom file directory for main site 
+		$upload_dir = wp_upload_dir();
+		$directory = $upload_dir['basedir'] . DIRECTORY_SEPARATOR . "CUSTOM_DIRECTORY_NAME" . DIRECTORY_SEPARATOR;
+		if (is_dir($directory)) {
+			foreach(glob($directory.'*.*') as $v){
+				unlink($v);
+			}
+			rmdir($directory);
+		}
+		*/
+	if ($blogs) {
+		foreach($blogs as $blog) {
+			switch_to_blog($blog['blog_id']);
+			/* @TODO: delete all transient, options and files you may have added 
+			delete_transient( 'TRANSIENT_NAME' );
+			delete_option('OPTION_NAME');
+			//info: remove custom file directory for main site 
+			$upload_dir = wp_upload_dir();
+			$directory = $upload_dir['basedir'] . DIRECTORY_SEPARATOR . "CUSTOM_DIRECTORY_NAME" . DIRECTORY_SEPARATOR;
+			if (is_dir($directory)) {
+				foreach(glob($directory.'*.*') as $v){
+					unlink($v);
+				}
+				rmdir($directory);
+			}
+			//info: remove and optimize tables
+			$GLOBALS['wpdb']->query("DROP TABLE `".$GLOBALS['wpdb']->prefix."TABLE_NAME`");
+			$GLOBALS['wpdb']->query("OPTIMIZE TABLE `" .$GLOBALS['wpdb']->prefix."options`");
+			*/
+			restore_current_blog();
+		}
+	}
+}
+else
+{
+	/* @TODO: delete all transient, options and files you may have added 
+	delete_transient( 'TRANSIENT_NAME' );
+	delete_option('OPTION_NAME');
+	//info: remove custom file directory for main site 
+	$upload_dir = wp_upload_dir();
+	$directory = $upload_dir['basedir'] . DIRECTORY_SEPARATOR . "CUSTOM_DIRECTORY_NAME" . DIRECTORY_SEPARATOR;
+	if (is_dir($directory)) {
+		foreach(glob($directory.'*.*') as $v){
+			unlink($v);
+		}
+		rmdir($directory);
+	}
+	//info: remove and optimize tables
+	$GLOBALS['wpdb']->query("DROP TABLE `".$GLOBALS['wpdb']->prefix."TABLE_NAME`");
+	$GLOBALS['wpdb']->query("OPTIMIZE TABLE `" .$GLOBALS['wpdb']->prefix."options`");
+	*/
+}
