624ffab01be194819e4a0341c4fdc66da1ed3f1c
Caity Ross <pandammonium@users.noreply.github.com>
2015-08-20 14:41:37 +0100

    Attempt 2 at fixing hierarchy
    Did it all wrong last time: should’ve kept the plugin-name directory
    outside the top-level directory instead of moving.  Should’ve moved the
    other things.

---
+++ includes/class-plugin-name-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ * @author     Your Name <email@example.com>
+ */
+class Plugin_Name_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ includes/class-plugin-name-i18n.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ * @author     Your Name <email@example.com>
+ */
+class Plugin_Name_i18n {
+
+	/**
+	 * The domain specified for this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $domain    The domain identifier for this plugin.
+	 */
+	private $domain;
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			$this->domain,
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+	/**
+	 * Set the domain equal to that of the specified domain.
+	 *
+	 * @since    1.0.0
+	 * @param    string    $domain    The domain that represents the locale of this plugin.
+	 */
+	public function set_domain( $domain ) {
+		$this->domain = $domain;
+	}
+
+}
