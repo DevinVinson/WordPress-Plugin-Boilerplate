5d17086151c2cdec2d5fae87679d73d0d94dafcb
AntoineIM <antoinep@interaction-multimedia.com>
2015-11-03 10:51:44 +0100

    Optional parameters

--- plugin-name/includes/class-plugin-name-loader.php
+++ plugin-name/includes/class-plugin-name-loader.php
@@ -113,8 +113,8 @@ class Plugin_Name_Loader {
 	 * @param    string               $hook             The name of the WordPress filter that is being registered.
 	 * @param    object               $component        A reference to the instance of the object on which the filter is defined.
 	 * @param    string               $callback         The name of the function definition on the $component.
-	 * @param    int                  $priority         The priority at which the function should be fired.
-	 * @param    int                  $accepted_args    The number of arguments that should be passed to the $callback.
+	 * @param    int                  $priority         Optional. The priority at which the function should be fired.
+	 * @param    int                  $accepted_args    Optional. The number of arguments that should be passed to the $callback.
 	 * @return   array                                  The collection of actions and filters registered with WordPress.
 	 */
 	private function add( $hooks, $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
