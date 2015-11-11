fcd4669f3521fb8020429471f6b98397d62336af
Piero Savastano <piero.savastano@gmail.com>
2015-11-11 16:33:27 +0100

    Update plugin-name-admin.js
    typo in the comment

--- plugin-name/admin/js/plugin-name-admin.js
+++ plugin-name/admin/js/plugin-name-admin.js
@@ -2,7 +2,7 @@
 	'use strict';
 
 	/**
-	 * All of the code for your public-facing JavaScript source
+	 * All of the code for your admin-facing JavaScript source
 	 * should reside in this file.
 	 *
 	 * Note: It has been assumed you will write jQuery code here, so the
