d6d2885441449433f09a900a42a60a661d365bc3
Cristi Citea <cristi.catea@gmail.com>
2015-11-25 12:34:33 -0800

    Adding menu hooks

--- plugin-name/includes/class-plugin-name.php
+++ plugin-name/includes/class-plugin-name.php
@@ -155,6 +155,8 @@ class Plugin_Name {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 
+		$this->loader->add_action( 'admin_menu', $plugin_admin, 'add_options_page' );
+		$this->loader->add_action( 'admin_menu', $plugin_admin, 'add_submenu_page' );
 	}
 
 	/**
