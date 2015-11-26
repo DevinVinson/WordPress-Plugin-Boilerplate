579fb9823a55f49e1db562aee9565b4446dd1cdc
Tal Ater <tal@talater.com>
2015-11-25 20:48:25 -0500

    Fixed mistake in comment.

--- plugin-name/public/class-plugin-name-public.php
+++ plugin-name/public/class-plugin-name-public.php
@@ -14,7 +14,7 @@
  * The public-facing functionality of the plugin.
  *
  * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the admin-specific stylesheet and JavaScript.
+ * enqueue the public-facing stylesheet and JavaScript.
  *
  * @package    Plugin_Name
  * @subpackage Plugin_Name/public
