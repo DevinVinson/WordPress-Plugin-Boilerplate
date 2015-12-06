0c099e42085f7a3a14e22ae372a0e29754e49259
Devin Vinson <devin@ran.ge>
2015-12-05 20:46:30 -0500

    Use plugin-name directly instead of passing a variable through
    This should fix the incorrect pattern of using a variable where we
    should be using the plugin-name instead. Same goes for any translatable
    strings within the plugin (no more using $this->plugin-name etc).

--- plugin-name/includes/class-plugin-name-i18n.php
+++ plugin-name/includes/class-plugin-name-i18n.php
@@ -26,14 +26,6 @@
  */
 class Plugin_Name_i18n {
 
-	/**
-	 * The domain specified for this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $domain    The domain identifier for this plugin.
-	 */
-	private $domain;
 
 	/**
 	 * Load the plugin text domain for translation.
@@ -43,21 +35,13 @@ class Plugin_Name_i18n {
 	public function load_plugin_textdomain() {
 
 		load_plugin_textdomain(
-			$this->domain,
+			'plugin-name',
 			false,
 			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
 		);
 
 	}
 
-	/**
-	 * Set the domain equal to that of the specified domain.
-	 *
-	 * @since    1.0.0
-	 * @param    string    $domain    The domain that represents the locale of this plugin.
-	 */
-	public function set_domain( $domain ) {
-		$this->domain = $domain;
-	}
+
 
 }
--- plugin-name/includes/class-plugin-name.php
+++ plugin-name/includes/class-plugin-name.php
@@ -135,7 +135,6 @@ class Plugin_Name {
 	private function set_locale() {
 
 		$plugin_i18n = new Plugin_Name_i18n();
-		$plugin_i18n->set_domain( $this->get_plugin_name() );
 
 		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
 
