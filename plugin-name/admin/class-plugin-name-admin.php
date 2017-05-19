af749912c74ae6bc5b32097ce03c206f5e23e75d
Clifford <tko@tourkick.com>
2017-05-19 16:10:41 -0500

    minor spacing/alignment corrections

--- plugin-name/admin/class-plugin-name-admin.php
+++ plugin-name/admin/class-plugin-name-admin.php
@@ -44,13 +44,13 @@ class Plugin_Name_Admin {
 	 * Initialize the class and set its properties.
 	 *
 	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
+	 * @param    string    $plugin_name    The name of this plugin.
+	 * @param    string    $version        The version of this plugin.
 	 */
 	public function __construct( $plugin_name, $version ) {
 
 		$this->plugin_name = $plugin_name;
-		$this->version = $version;
+		$this->version     = $version;
 
 	}
 
