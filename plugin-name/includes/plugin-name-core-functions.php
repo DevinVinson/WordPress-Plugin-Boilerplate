37a8769d8971c15eb6286af4e375fdcc3b260454
Paulo Iankoski <me@wpdev.com.br>
2017-06-05 12:27:18 -0300

    Suggestion of new structure.

---
+++ plugin-name/includes/plugin-name-core-functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * WordPress Plugin Boilerplate Core Functions
+ *
+ * General core functions available on both the front-end and admin.
+ *
+ * @author 		Your Name or Your Company
+ * @category 	Core
+ * @package 	Plugin_Name/Functions
+ * @version     1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Include core functions (available in both admin and frontend).
+include( 'plugin-name-template-functions.php' );
+
+// IMPLEMENT YOUR HELPER FUNCTIONS HERE
--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -18,7 +18,7 @@
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
  * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author URI:        http://example.com/?
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       plugin-name
@@ -26,52 +26,238 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-plugin-name-activator.php
- */
-function activate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
-	Plugin_Name_Activator::activate();
-}
+if ( ! class_exists( 'Plugin_Name' ) ) :
+
+	final class Plugin_Name {
+
+		/**
+	 * Plugin_Name version.
+	 *
+	 * @var string
+	 */
+		public $version = '1.0.0';
+
+		/**
+	 * The single instance of the class.
+	 *
+	 * @var Plugin_Name
+	 * @since 1.0.0
+	 */
+		protected static $_instance = null;
+
+		/**
+	 * Main Plugin_Name Instance.
+	 *
+	 * Ensures only one instance of Plugin_Name is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 * @static
+	 * @see PName()
+	 * @return Plugin_Name - Main instance.
+	 */
+		public static function instance() {
+			if ( is_null( self::$_instance ) ) {
+				self::$_instance = new self();
+			}
+			return self::$_instance;
+		}
+
+		/**
+	 * Cloning is forbidden.
+	 * @since 1.0.0
+	 */
+		public function __clone() {
+			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'plugin-name' ), '1.0.0' );
+		}
+
+		/**
+	 * Unserializing instances of this class is forbidden.
+	 * @since 1.0.0
+	 */
+		public function __wakeup() {
+			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'plugin-name' ), '1.0.0' );
+		}
+
+		/**
+	 * Plugin_Name Constructor.
+	 */
+		public function __construct() {
+			$this->define_constants();
+			$this->includes();
+			$this->init_hooks();
+
+			do_action( 'plugin_name_loaded' );
+		}
+
+		/**
+	 * Hook into actions and filters.
+	 * @since  1.0.0
+	 */
+		private function init_hooks() {
+			register_activation_hook( __FILE__, array( 'PName_Install', 'install' ) );
+			add_action( 'init', array( $this, 'init' ), 0 );
+		}
+
+		/**
+	 * Define PName Constants.
+	 */
+		private function define_constants() {
+			$upload_dir = wp_upload_dir();
+
+			$this->define( 'PNAME_PLUGIN_FILE', __FILE__ );
+			$this->define( 'PNAME_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+			$this->define( 'PNAME_VERSION', $this->version );
+		}
+
+		/**
+	 * Define constant if not already set.
+	 *
+	 * @param  string $name
+	 * @param  string|bool $value
+	 */
+		private function define( $name, $value ) {
+			if ( ! defined( $name ) ) {
+				define( $name, $value );
+			}
+		}
+
+		/**
+	 * What type of request is this?
+	 *
+	 * @param  string $type admin, ajax, cron or frontend.
+	 * @return bool
+	 */
+		private function is_request( $type ) {
+			switch ( $type ) {
+				case 'admin' :
+					return is_admin();
+				case 'ajax' :
+					return defined( 'DOING_AJAX' );
+				case 'cron' :
+					return defined( 'DOING_CRON' );
+				case 'frontend' :
+					return ( ! is_admin() || defined( 'DOING_AJAX' ) ) && ! defined( 'DOING_CRON' );
+			}
+		}
+
+		/**
+	 * Include required core files used in admin and on the frontend.
+	 */
+		public function includes() {
+			include_once( 'includes/class-plugin-name-autoloader.php' );
+			include_once( 'includes/plugin-name-core-functions.php' );
+			include_once( 'includes/class-plugin-name-install.php' );
+			include_once( 'includes/class-plugin-name-ajax.php' );
+
+			if ( $this->is_request( 'admin' ) ) {
+				include_once( 'includes/admin/class-plugin-name-admin.php' );
+			}
+
+			if ( $this->is_request( 'frontend' ) ) {
+				$this->frontend_includes();
+			}
+
+			include_once( 'includes/class-plugin-name-post-types.php' ); // Registers post types
+
+			$this->customizations_includes();
+		}
+
+		/**
+	 * Include required frontend files.
+	 */
+		public function frontend_includes() {
+			include_once( 'includes/class-plugin-name-template-loader.php' );  // Template Loader
+			include_once( 'includes/class-plugin-name-frontend-scripts.php' ); // Frontend Scripts
+			include_once( 'includes/class-plugin-name-form-handler.php' );     // Form Handlers
+		}
+
+		/**
+	 * Include required customizations files.
+	 */
+		public function customizations_includes() {
+			include_once( 'includes/customizations/class-plugin-name-template-loader.php' );  // Template Loader
+		}
+
+		/**
+	 * Init Plugin_Name when WordPress Initialises.
+	 */
+		public function init() {
+			// Before init action.
+			do_action( 'before_plugin_name_init' );
+
+			// Set up localisation.
+			$this->load_plugin_textdomain();
+
+			// Init action.
+			do_action( 'plugin_name_init' );
+		}
+
+		/**
+	 * Load Localisation files.
+	 *
+	 * Note: the first-loaded translation file overrides any following ones if the same translation is present.
+	 *
+	 * Locales found in:
+	 *      - WP_LANG_DIR/plugin-name/plugin-name-LOCALE.mo
+	 *      - WP_LANG_DIR/plugins/plugin-name-LOCALE.mo
+	 */
+		public function load_plugin_textdomain() {
+			$locale = apply_filters( 'plugin_locale', get_locale(), 'plugin-name' );
+
+			load_textdomain( 'plugin-name', WP_LANG_DIR . '/plugin-name/plugin-name-' . $locale . '.mo' );
+			load_plugin_textdomain( 'plugin-name', false, plugin_basename( dirname( __FILE__ ) ) . '/i18n/languages' );
+		}
+
+		/**
+	 * Get the plugin url.
+	 * @return string
+	 */
+		public function plugin_url() {
+			return untrailingslashit( plugins_url( '/', __FILE__ ) );
+		}
+
+		/**
+	 * Get the plugin path.
+	 * @return string
+	 */
+		public function plugin_path() {
+			return untrailingslashit( plugin_dir_path( __FILE__ ) );
+		}
+
+		/**
+	 * Get the template path.
+	 * @return string
+	 */
+		public function template_path() {
+			return apply_filters( 'plugin_name_template_path', 'plugin-name/' );
+		}
+
+		/**
+	 * Get Ajax URL.
+	 * @return string
+	 */
+		public function ajax_url() {
+			return admin_url( 'admin-ajax.php', 'relative' );
+		}
+	}
+
+endif;
 
 /**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-plugin-name-deactivator.php
- */
-function deactivate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
-	Plugin_Name_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_plugin_name' );
-register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
-
-/**
- * This simple function is globally available to get the plugin's object.
+ * Main instance of Plugin_Name.
+ *
+ * Returns the main instance of PName to prevent the need to use globals.
+ *
+ * @since  1.0.0
+ * @return Plugin_Name
  */
 function PName() {
 	return Plugin_Name::instance();
 }
 
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-
-PName();
+// Global for backwards compatibility.
+$GLOBALS['plugin_name'] = PName();
