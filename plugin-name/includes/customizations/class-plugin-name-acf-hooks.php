16a9e50e96bdef837ea536a83e45885f3a2fca2c
Paulo Iankoski <me@wpdev.com.br>
2017-06-06 18:08:25 -0300

    Change indentations following WP standards

--- plugin-name/includes/customizations/class-plugin-name-acf-hooks.php
+++ plugin-name/includes/customizations/class-plugin-name-acf-hooks.php
@@ -14,43 +14,43 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 if ( ! class_exists( 'PName_ACF_Hooks' ) ) :
 
-/**
- * PName_ACF_Hooks Class.
- */
-class PName_ACF_Hooks {
-
 	/**
-	 * Hook in methods.
+	 * PName_ACF_Hooks Class.
 	 */
-	public function __construct() {
-		// add_filter( 'acf/settings/load_json', array( $this, 'acf_json_load_point' ) );
-		// add_filter( 'acf/settings/save_json', array( $this, 'acf_json_save_point' ) );
+	class PName_ACF_Hooks {
+
+		/**
+		 * Hook in methods.
+		 */
+		public function __construct() {
+			// add_filter( 'acf/settings/load_json', array( $this, 'acf_json_load_point' ) );
+			// add_filter( 'acf/settings/save_json', array( $this, 'acf_json_save_point' ) );
+		}
+
+		/**
+		 * Set from where ACF must load JSON files
+		 * @param  [array] $paths
+		 * @return [array]
+		 */
+		public function acf_json_load_point( $paths ) {
+			unset( $paths[0] );
+
+			$paths[] = dirname( __FILE__ ) . '/acf-json';
+
+			return $paths;
+		}
+
+		/**
+		 * Set to where ACF must save JSON files
+		 * @param  [string] $path
+		 * @return [string]
+		 */
+		public function acf_json_save_point( $path ) {
+			$path = dirname( __FILE__ ) . '/acf-json';
+
+			return $path;
+		}
 	}
-
-	/**
-	 * Set from where ACF must load JSON files
-	 * @param  [array] $paths
-	 * @return [array]
-	 */
-	public function acf_json_load_point( $paths ) {
-		unset( $paths[0] );
-
-		$paths[] = dirname( __FILE__ ) . '/acf-json';
-
-		return $paths;
-	}
-
-	/**
-	 * Set to where ACF must save JSON files
-	 * @param  [string] $path
-	 * @return [string]
-	 */
-	public function acf_json_save_point( $path ) {
-		$path = dirname( __FILE__ ) . '/acf-json';
-
-		return $path;
-	}
-}
 
 endif;
 
