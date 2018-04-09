dbe49eacd65d0f9c4caa64db98eff1eed6cd2505
Matias Saggiorato <matiassaggiorato@gmail.com>
2018-04-09 19:47:10 -0300

    Fix text domain path

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -22,7 +22,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       plugin-name
- * Domain Path:       /languages
+ * Domain Path:       /i18n/languages
  */
 
 // If this file is called directly, abort.
