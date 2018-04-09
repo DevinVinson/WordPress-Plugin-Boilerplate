cc3454ddd29d591d9c51b74896620be1c59719c6
Matias Saggiorato <matiassaggiorato@gmail.com>
2018-04-09 19:49:44 -0300

    Remove question mark at Author URI.

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -18,7 +18,7 @@
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
  * Author:            Your Name or Your Company
- * Author URI:        http://example.com/?
+ * Author URI:        http://example.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       plugin-name
