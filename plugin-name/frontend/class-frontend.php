5915f572fabe59bdc7640faf4aad41124307cb23
Brian Henry <BrianHenryIE@gmail.com>
2020-01-16 15:18:55 -0800

    Run PHP Code Beautifier and Fixer

--- plugin-name/frontend/class-frontend.php
+++ plugin-name/frontend/class-frontend.php
@@ -46,13 +46,13 @@ class Frontend {
 	 * Initialize the class and set its properties.
 	 *
 	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of the plugin.
-	 * @param      string    $version    The version of this plugin.
+	 * @param      string $plugin_name       The name of the plugin.
+	 * @param      string $version    The version of this plugin.
 	 */
 	public function __construct( $plugin_name, $version ) {
 
 		$this->plugin_name = $plugin_name;
-		$this->version = $version;
+		$this->version     = $version;
 
 	}
 
