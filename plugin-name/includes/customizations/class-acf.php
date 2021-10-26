070d2b5824849e773c3676fdcc6489ea31a41011
Matias Saggiorato <matiassaggiorato@gmail.com>
2021-10-26 20:00:43 -0300

    Remove unnecessary if

--- plugin-name/includes/customizations/class-acf.php
+++ plugin-name/includes/customizations/class-acf.php
@@ -12,46 +12,42 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! class_exists( 'ACF' ) ) :
+/**
+ * ACF Class.
+ */
+class ACF {
 
 	/**
-	 * ACF Class.
+	 * Hook in methods.
 	 */
-	class ACF {
-
-		/**
-		 * Hook in methods.
-		 */
-		public static function init() {
-			// add_filter( 'acf/settings/load_json', array( __CLASS__, 'acf_json_load_point' ) );
-			// add_filter( 'acf/settings/save_json', array( __CLASS__, 'acf_json_save_point' ) );
-		}
-
-		/**
-		 * Set from where ACF must load JSON files
-		 *
-		 * @param  array $paths Paths that ACF will read JSON from.
-		 * @return array
-		 */
-		public static function acf_json_load_point( $paths ) {
-			unset( $paths[0] );
-
-			$paths[] = dirname( __FILE__ ) . '/acf-json';
-
-			return $paths;
-		}
-
-		/**
-		 * Set to where ACF must save JSON files
-		 *
-		 * @param  string $path Path that ACF will save JSON to.
-		 * @return string
-		 */
-		public static function acf_json_save_point( $path ) {
-			$path = dirname( __FILE__ ) . '/acf-json';
-
-			return $path;
-		}
+	public static function init() {
+		// add_filter( 'acf/settings/load_json', array( __CLASS__, 'acf_json_load_point' ) );
+		// add_filter( 'acf/settings/save_json', array( __CLASS__, 'acf_json_save_point' ) );
 	}
 
-endif;
+	/**
+	 * Set from where ACF must load JSON files
+	 *
+	 * @param  array $paths Paths that ACF will read JSON from.
+	 * @return array
+	 */
+	public static function acf_json_load_point( $paths ) {
+		unset( $paths[0] );
+
+		$paths[] = dirname( __FILE__ ) . '/acf-json';
+
+		return $paths;
+	}
+
+	/**
+	 * Set to where ACF must save JSON files
+	 *
+	 * @param  string $path Path that ACF will save JSON to.
+	 * @return string
+	 */
+	public static function acf_json_save_point( $path ) {
+		$path = dirname( __FILE__ ) . '/acf-json';
+
+		return $path;
+	}
+}
