02f229ea24c485447b9f7dd194565b7d7eb6942b
atakan au <altun.atakan@gmail.com>
2022-07-28 22:55:34 +0300

    Rename files on WindowsOS
    Rename files on WindowsOS

---
+++ rename_win.bat
@@ -0,0 +1,20 @@
+REM ### find & replace: plugin-name > example-me ###
+
+ren plugin-name\admin\css\plugin-name-admin.css example-me-admin.css
+ren plugin-name\admin\js\plugin-name-admin.js example-me-admin.js
+ren plugin-name\admin\partials\plugin-name-admin-display.php example-me-admin-display.php
+ren plugin-name\admin\class-plugin-name-admin.php class-example-me-admin.php
+ren plugin-name\includes\class-plugin-name-activator.php class-example-me-activator.php
+ren plugin-name\includes\class-plugin-name-deactivator.php class-example-me-deactivator.php
+ren plugin-name\includes\class-plugin-name-i18n.php class-example-me-i18n.php
+ren plugin-name\includes\class-plugin-name-loader.php class-example-me-loader.php
+ren plugin-name\includes\class-plugin-name.php class-example-me.php
+ren plugin-name\languages\plugin-name.pot example-me.pot
+ren plugin-name\public\css\plugin-name-public.css example-me-public.css
+ren plugin-name\public\js\plugin-name-public.js example-me-public.js
+ren plugin-name\public\partials\plugin-name-public-display.php example-me-public-display.php
+ren plugin-name\public\class-plugin-name-public.php class-example-me-public.php
+ren plugin-name\plugin-name.php example-me.php
+ren plugin-name example-me
+
+pause
